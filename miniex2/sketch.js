3d4374e25a7a5d88702f3e5c2507ca2d5064f073
Emma Ottilie <emmaottilie@gmail.com>
2017-02-15 15:51:19 +0100

    Add files via upload

---
+++ miniex2/sketch.js
@@ -0,0 +1,73 @@
+function setup() {
+  createCanvas(800, 400)
+}
+
+function draw() {
+  background('#fae');
+  
+  textSize(16);
+  text("404", 240, 245);
+
+  noStroke()
+  ellipse(56, 45, 55, 55);
+  noStroke()
+  ellipse(156, 45, 55, 55);
+  noStroke()
+  ellipse(256, 45, 55, 55);
+  noStroke()
+  ellipse(356, 45, 55, 55);
+  noStroke()
+  ellipse(456, 45, 55, 55);
+  noStroke()
+  ellipse(556, 45, 55, 55);
+  noStroke()
+  ellipse(656, 45, 55, 55);
+  noStroke()
+  ellipse(756, 45, 55, 55);
+  noStroke()
+  ellipse(56, 145, 55, 55);
+  noStroke()
+  ellipse(156, 145, 55, 55);
+  noStroke()
+  ellipse(256, 145, 55, 55);
+  noStroke()
+  ellipse(356, 145, 55, 55);
+  noStroke()
+  ellipse(456, 145, 55, 55);
+  noStroke()
+  ellipse(556, 145, 55, 55);
+  noStroke()
+  ellipse(656, 145, 55, 55);
+  noStroke()
+  ellipse(756, 145, 55, 55);
+  noStroke()
+  ellipse(56, 245, 55, 55);
+  noStroke()
+  ellipse(156, 245, 55, 55);
+  noStroke()
+  ellipse(356, 245, 55, 55);
+  noStroke()
+  ellipse(456, 245, 55, 55);
+  noStroke()
+  ellipse(556, 245, 55, 55);
+  noStroke()
+  ellipse(654, 245, 55, 55);
+  noStroke()
+  ellipse(756, 245, 55, 55);
+  noStroke()
+  ellipse(56, 345, 55, 55);
+  noStroke()
+  ellipse(156, 345, 55, 55);
+  noStroke()
+  ellipse(256, 345, 55, 55);
+  noStroke()
+  ellipse(356, 345, 55, 55);
+  noStroke()
+  ellipse(456, 345, 55, 55);
+  noStroke()
+  ellipse(556, 345, 55, 55);
+  noStroke()
+  ellipse(656, 345, 55, 55);
+  noStroke()
+  ellipse(756, 345, 55, 55);
+}
